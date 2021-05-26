c84097c381a4f435c4f4cb84d0f57e2047bda64b
bbbradsmith <bbbradsmith@users.noreply.github.com>
2021-05-26 15:57:25 -0400

    reduce paddle timeout to 128 to cut the timeout time in half (5ms instead of 10ms)

--- a2f/paddle.s
+++ a2f/paddle.s
@@ -15,10 +15,10 @@
 
 .segment "CODE"
 paddle_buttons: .byte 0
-paddle0_x: .byte 255
-paddle0_y: .byte 255
-paddle1_x: .byte 255
-paddle1_y: .byte 255
+paddle0_x: .byte 128
+paddle0_y: .byte 128
+paddle1_x: .byte 128
+paddle1_y: .byte 128
 
 .assert paddle1_x = paddle0_x + 2, error, "paddle1 result must be 2 bytes after paddle0"
 .assert paddle1_y = paddle0_y + 2, error, "paddle1 result must be 2 bytes after paddle0"
@@ -28,7 +28,7 @@ paddle1_y: .byte 255
 .align 32
 .proc paddle_poll_aligned_
 	; X = 0 (paddle 0) or 2 (paddle 1)
-	; Y = 1 to ensure timeout before overflow
+	; Y = 0
 @loop: ; 43 cycle loop
 	; X axis increment
 	lda $C064, X
@@ -47,7 +47,7 @@ paddle1_y: .byte 255
 	ora $C065, X
 	bpl @finish
 	iny
-	bne @loop
+	bpl @loop ; timeout at 128
 	.assert >(@loop) = >*, error, "Page crossed!"
 	.assert (*-@loop)<32, error, "paddle_poll_aligned_ loop may not cross 32 byte alignment."
 	; NOTE: alignment isn't critical for this to work, but it gives a tiny bit of extra consistency
@@ -67,28 +67,29 @@ paddle_buttons_poll:
 	asl $C061
 	rol paddle_buttons
 	lda paddle_buttons
-	rts
+	rts ; result in X:A for C calls
 
 paddle01_poll:
 	ldx #2
 	jsr paddle_poll_ ; poll axes 2,3 (X=2)
 paddle0_poll:
+	; poll buttons first to allow extra discharge time before next read
 	jsr paddle_buttons_poll ; NOTE: result X=0
 	; poll axes 0,1 (X=0)
 paddle_poll_:
 	; pre-charge read: make sure both axes have returned to 0 (or timeout)
-	ldy #1
+	ldy #0
 	jsr paddle_poll_aligned_
-	cpy #0
-	beq @timeout ; Y=0 indicates timeout, likely no paddle attached
-	; charge capacitors
+	cpy #128
+	bcs @timeout ; Y=128 indicates timeout, likely no paddle attached
+	; charge capacitors (NOTE: PREAD waits at least 6 cycles before first poll after charge)
 	lda $C070
 	; reset counters
 	ldy #0
 	sty a2f_temp+0
 	sty a2f_temp+1
 	; read the axes
-	iny ; Y=1 so timeouts will stop at 255
+	; Y=0
 	jsr paddle_poll_aligned_
 	; store the result
 	lda a2f_temp+0
@@ -97,7 +98,7 @@ paddle_poll_:
 	sta paddle0_y, X
 	rts
 @timeout:
-	lda #255
+	tya
 	sta paddle0_x, X
 	sta paddle0_y, X
 	rts
