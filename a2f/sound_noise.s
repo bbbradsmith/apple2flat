468255919a123922acec34086b313d5ef76e28a1
bbbradsmith <bbbradsmith@users.noreply.github.com>
2021-06-23 06:00:44 -0400

    speaker parity preservation for sound_noise

--- a2f/sound_noise.s
+++ a2f/sound_noise.s
@@ -23,46 +23,48 @@ sound_noise_seed: .word $5555
 
 .align 32
 
-; a2f_temp 1:0 = cycles-40 for first half of pulse
+; a2f_temp 1:0 = cycles-45 for first half of pulse
 ; a2f_temp 4:Y = wave count to play
 .proc sound_noise_
 @loop:
-	lda a2f_temp+2   ; +3
-	asl              ; +2 = 5
-	rol a2f_temp+3   ; +5 = 10
-	BP bcs, :+       ; +2 = 12 (+3 = 13)
-		bit $C030    ; +4 = 16
-		jmp :++      ; +3 = 19
+	lda a2f_temp+2       ; +3
+	asl                  ; +2 = 5
+	rol a2f_temp+3       ; +5 = 10
+	BP bcs, :+           ; +2 = 12 (+3 = 13)
+		bit $C030        ; +4 = 16
+		inc a2f_temp+5   ; +5 = 21
+		jmp :++          ; +3 = 24
 	:
-		eor #$39     ; +2 = 15
-		nop          ; +2 = 17
-		nop          ; +2 = 19
+		eor #$39         ; +2 = 15
+		php              ; +3 = 18
+		plp              ; +4 = 22
+		nop              ; +2 = 24
 	:
-	sta a2f_temp+2   ; +3 = 22
-	lda a2f_temp+0   ; +3 = 25
-	ldx a2f_temp+1   ; +3 = 28
-	jsr vdelay       ; (X:A cycles)
-	dey              ; +2 = 30
-	BP bne, :+       ; +2 = 32 (+3 = 33)
-	dec a2f_temp+4   ; +5 = 37
-	BP bpl, @loop    ; +3 = 40 (second half overhead, branch 1)
+	sta a2f_temp+2       ; +3 = 27
+	lda a2f_temp+0       ; +3 = 30
+	ldx a2f_temp+1       ; +3 = 33
+	jsr vdelay           ; (X:A cycles)
+	dey                  ; +2 = 35
+	BP bne, :+           ; +2 = 37 (+3 = 38)
+	dec a2f_temp+4       ; +5 = 42
+	BP bpl, @loop        ; +3 = 45 (second half overhead, branch 1)
 	rts
 :
-	nop              ; +2 = 35
-	nop              ; +2 = 37
-	jmp @loop        ; +3 = 40 (second half overhead, branch 2)
+	nop                  ; +2 = 40
+	nop                  ; +2 = 42
+	jmp @loop            ; +3 = 45 (second half overhead, branch 2)
 .endproc
 
 .segment "CODE"
 
 .proc sound_noise
-	; 40 cycle overhead
+	; 45 cycle overhead
 	lda a2f_temp+0
 	sec
-	sbc #<40
+	sbc #<45
 	sta a2f_temp+0
 	lda a2f_temp+1
-	sbc #>40
+	sbc #>45
 	sta a2f_temp+1
 	; copy seed to ZP
 	lda sound_noise_seed+0
@@ -70,7 +72,18 @@ sound_noise_seed: .word $5555
 	sta a2f_temp+2
 	lda sound_noise_seed+1
 	sta a2f_temp+3
-	jsr sound_noise_ ; do noise
+	; count parity
+	lda #0
+	sta a2f_temp+5
+	; do noise
+	jsr sound_noise_
+	; if noise flipped the speaker an odd number of times, flip it once more for parity
+	lda a2f_temp+5
+	and #1
+	beq :+
+		bit $C030
+	:
+	; copy ZP back to seed
 	lda a2f_temp+2
 	sta sound_noise_seed+0
 	lda a2f_temp+3
